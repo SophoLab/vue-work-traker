deeb4e3e12c3d10910a4e4819c1691f65c8fd8b7
Samuel Bouye <bouyesamuel@gmail.com>
2022-02-17 12:38:18 +0000

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,29 @@
+<script>
+      export default {
+        data() {
+          return {
+            msg : 'Hello From Vuejs Planet',
+            count : 0
+          }
+        },
+        methods : {
+          subOne(){
+            this.count > 0 ? this.count-- : null
+          }
+        }
+      }
+</script>
+
+<template>
+  <div class="main">
+    {{ msg }}
+    <h3>Count {{count}}</h3>
+    <button @click="count++">Add 1</button>
+    <button @click="subOne">Sub 1</button>
+    <button @click="count = 0">Reset</button>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
