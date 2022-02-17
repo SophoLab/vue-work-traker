7e819f24dbce58afbeab8f590dec97ecd307b24e
Samuel Bouye <bouyesamuel@gmail.com>
2022-02-17 14:01:59 +0000

    delete unused element

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
     <main>
-    <HelloWorld/>
+    <h1>Hello Wolrd</h1>
   </main>
 </template>
 
 <style>
-/* @import './assets/base.css'; */
 
 
 </style>
