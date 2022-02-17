774562a6507e856770f873bca880600d5d21330c
Samuel Bouye <bouyesamuel@gmail.com>
2022-02-17 15:43:30 +0000

    delete a todo

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -2,14 +2,20 @@
   <div class="todo-list">
       <h2 v-show="todos.length == 0">This list is empty</h2>
       <ul v-for="todo in todos" :key="todo">
-          <li>{{todo.content}}</li>
+          <li>{{todo.content}} <button @click="getTodo(todo)">X</button></li>
       </ul>
   </div>
 </template>
 
 <script>
 export default {
-    props: ['todos']
+    props: ['todos'],
+    methods: {
+        getTodo(todo){
+            const index = this.todos.indexOf(todo)
+            this.todos.splice(index, 1)
+        }
+    }
 }
 </script>
 
